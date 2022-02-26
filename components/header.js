8e3b64a2110f8a3951b17865bf93e2a4a3e282f0
Shin Yi <contact.shinyi@gmail.com>
2022-02-26 14:16:37 +0800

    test logout bug

--- components/header.js
+++ components/header.js
@@ -16,7 +16,7 @@ const Header = () => {
     localStorage.clear();
     userLoginState.setLoginState(false);
     setUserRole("");
-    router.push("/");
+    // router.push("/");
   };
 
   const decodeToken = () => {
